f519d1cf744f9e74525b268a077b94d548704cc7
Alexey Gladkov <legion@kernel.org>
2024-11-15 11:15:04 +0100

    Unify subcommands creation
    There is no need to create a new variable for subcommand each time.
    
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- src/lkvm/command.py
+++ src/lkvm/command.py
@@ -80,104 +80,104 @@ The program to run virtual machines and run programs in their virtual environmen
     subparsers = parser.add_subparsers(dest="subcmd", help="")
 
     # command: setup
-    sp0_description = """\
+    sp_description = """\
 Setup a new virtual machine. This creates a new rootfs in the .vm folder
 of your home directory.
 
 """
-    sp0 = subparsers.add_parser("setup",
-                                formatter_class=argparse.RawTextHelpFormatter,
-                                description=sp0_description, help=sp0_description,
-                                epilog=epilog, add_help=False)
-    sp0.set_defaults(func=cmd_setup)
-    add_common_arguments(sp0)
+    sp = subparsers.add_parser("setup",
+                               formatter_class=argparse.RawTextHelpFormatter,
+                               description=sp_description, help=sp_description,
+                               epilog=epilog, add_help=False)
+    sp.set_defaults(func=cmd_setup)
+    add_common_arguments(sp)
 
     mode_choices = ["9p", "disk"]
 
     if lkvm.HAVE_NFS:
         mode_choices.append("nfs")
 
-    sp0.add_argument("-m", "--mode",
+    sp.add_argument("-m", "--mode",
                      dest="mode", action="store", default="9p", choices=mode_choices,
                      help="profile mode (default: %(default)s).")
-    sp0.add_argument("profile", help="name of profile")
+    sp.add_argument("profile", help="name of profile")
 
     # command: list
-    sp1_description = """\
+    sp_description = """\
 Print a list of running instances on the host. This is restricted to instances
 started by the current user, as it looks in the .vm folder in your home
 directory.
 
 """
-    sp1 = subparsers.add_parser("list",
-                                formatter_class=argparse.RawTextHelpFormatter,
-                                description=sp1_description, help=sp1_description,
-                                epilog=epilog, add_help=False)
-    sp1.set_defaults(func=cmd_list)
-    add_common_arguments(sp1)
+    sp = subparsers.add_parser("list",
+                               formatter_class=argparse.RawTextHelpFormatter,
+                               description=sp_description, help=sp_description,
+                               epilog=epilog, add_help=False)
+    sp.set_defaults(func=cmd_list)
+    add_common_arguments(sp)
 
     # command: run
-    sp2_description = """\
-Starts a virtual machine according to specified profile.
+    sp_description = """\
+            Starts a virtual machine according to specified profile.
 
 """
-    sp2 = subparsers.add_parser("run",
-                                formatter_class=argparse.RawTextHelpFormatter,
-                                description=sp2_description, help=sp2_description,
-                                epilog=epilog, add_help=False)
-    sp2.set_defaults(func=cmd_run)
-    add_common_arguments(sp2)
-    sp2.add_argument('-n', '--dry-run',
+    sp = subparsers.add_parser("run",
+                               formatter_class=argparse.RawTextHelpFormatter,
+                               description=sp_description, help=sp_description,
+                               epilog=epilog, add_help=False)
+    sp.set_defaults(func=cmd_run)
+    add_common_arguments(sp)
+    sp.add_argument('-n', '--dry-run',
                      dest="dry_run", action='store_true', default=False,
                      help='show what should be launched.')
-    add_qemu_arguments(sp2)
-    sp2.add_argument("profile", help="name of profile")
+    add_qemu_arguments(sp)
+    sp.add_argument("profile", help="name of profile")
 
     # command: sandbox
-    sp3_description = """\
+    sp_description = """\
 Runs a command in a sandboxed guest. vm will inject a special init binary which
 will do an initial setup of the guest Linux and then lauch a shell script with
 the specified command. Upon this command ending, the guest will be shutdown.
 
 """
-    sp3 = subparsers.add_parser("sandbox",
-                                formatter_class=argparse.RawTextHelpFormatter,
-                                description=sp3_description, help=sp3_description,
-                                epilog=epilog, add_help=False)
-    sp3.set_defaults(func=cmd_sandbox)
-    add_common_arguments(sp3)
-    sp3.add_argument('-n', '--dry-run',
+    sp = subparsers.add_parser("sandbox",
+                               formatter_class=argparse.RawTextHelpFormatter,
+                               description=sp_description, help=sp_description,
+                               epilog=epilog, add_help=False)
+    sp.set_defaults(func=cmd_sandbox)
+    add_common_arguments(sp)
+    sp.add_argument('-n', '--dry-run',
                      dest="dry_run", action='store_true', default=False,
                      help='show what should be launched.')
-    add_qemu_arguments(sp3)
-    sp3.add_argument("profile", help="name of profile")
-    sp3.add_argument("prog", help="script when booting into custom rootfs")
-    sp3.add_argument("args", nargs='*', help="optional <prog> arguments")
+    add_qemu_arguments(sp)
+    sp.add_argument("profile", help="name of profile")
+    sp.add_argument("prog", help="script when booting into custom rootfs")
+    sp.add_argument("args", nargs='*', help="optional <prog> arguments")
 
     # command: crash
-    sp4_description = """\
+    sp_description = """\
 Controls the VM state (start, stop, pause, etc.).
 
 """
-    sp4 = subparsers.add_parser("vm",
-                                formatter_class=argparse.RawTextHelpFormatter,
-                                description=sp4_description, help=sp4_description,
-                                epilog=epilog, add_help=False)
-    sp4.set_defaults(func=cmd_vm)
-    add_common_arguments(sp4)
-    sp4.add_argument('--stop',
+    sp = subparsers.add_parser("vm",
+                               formatter_class=argparse.RawTextHelpFormatter,
+                               description=sp_description, help=sp_description,
+                               epilog=epilog, add_help=False)
+    sp.set_defaults(func=cmd_vm)
+    add_common_arguments(sp)
+    sp.add_argument('--stop',
                      dest="vm_state", action='store_const', const="stop",
                      help='Stop guest VM execution.')
-    sp4.add_argument('--continue',
+    sp.add_argument('--continue',
                      dest="vm_state", action='store_const', const="continue",
                      help='Resume guest VM execution.')
-    sp4.add_argument('--quit',
+    sp.add_argument('--quit',
                      dest="vm_state", action='store_const', const="quit",
                      help='QEMU process to exit gracefully.')
-    sp4.add_argument('--dump-memory',
+    sp.add_argument('--dump-memory',
                      dest="dump_memory", action='store', metavar='FILE',
                      help='Dump guest VM memory to FILE.')
-    sp4.add_argument("profile", help="name of profile")
+    sp.add_argument("profile", help="name of profile")
 
     return parser
 
